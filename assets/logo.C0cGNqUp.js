9f0c00bb98fc137a58cbc4a1868e6ad16589b235
hsmdygz <2023242380@qq.com>
2025-05-16 18:06:37 +0800

    Updates

--- assets/logo.C0cGNqUp.js
+++
@@ -1,1 +0,0 @@
-const o="/keyanzhushou/static/logo.png";export{o as _};
--- assets/user.Cx3nd9dg.js
+++
@@ -1,1 +0,0 @@
-import{h as s}from"./request.DmWrvmgp.js";const r=(r,e)=>s.post("/user/login",{username:r,password:e}),e=(r,e,o)=>s.post("/user/register",{username:r,email:e,password:o});export{r as l,e as r};
